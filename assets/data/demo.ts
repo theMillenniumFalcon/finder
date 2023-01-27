faaacfff0ae4214547bd307ff7d38aba52d25d9a
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-01-27 13:53:24 +0530

    added types

--- assets/data/demo.ts
+++ assets/data/demo.ts
@@ -1,14 +1,14 @@
-import { DataT } from "../../types";
-import IMAGE_01 from "../images/01.jpg";
-import IMAGE_02 from "../images/02.jpg";
-import IMAGE_03 from "../images/03.jpg";
-import IMAGE_04 from "../images/04.jpg";
-import IMAGE_05 from "../images/05.jpg";
-import IMAGE_06 from "../images/06.jpg";
-import IMAGE_07 from "../images/07.jpg";
-import IMAGE_08 from "../images/08.jpg";
-import IMAGE_09 from "../images/09.jpg";
-import IMAGE_10 from "../images/10.jpg";
+import { DataT } from "../../types"
+import IMAGE_01 from "../images/01.jpg"
+import IMAGE_02 from "../images/02.jpg"
+import IMAGE_03 from "../images/03.jpg"
+import IMAGE_04 from "../images/04.jpg"
+import IMAGE_05 from "../images/05.jpg"
+import IMAGE_06 from "../images/06.jpg"
+import IMAGE_07 from "../images/07.jpg"
+import IMAGE_08 from "../images/08.jpg"
+import IMAGE_09 from "../images/09.jpg"
+import IMAGE_10 from "../images/10.jpg"
 
 const data: DataT[] = [
   {
@@ -124,6 +124,6 @@ const data: DataT[] = [
       "You want order in Gotham. Batman must take off his mask and turn himself in.",
     image: IMAGE_10,
   },
-];
+]
 
-export default data;
+export default data
